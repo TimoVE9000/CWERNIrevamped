dde5eadad35a414dc186a623ecc85dcb8e8073bb
TimoVE9000 <timovaneldijk@gmail.com>
2025-12-04 13:48:45 +0100

    added simbvar function #10

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -12,7 +12,7 @@ test_that("test generate_spat_abund", {
 
 
 test_that("test nulsim", {
-  result = nulsim(10, 0.6, 0.1, 16000, generate_spat_abund(theta = 200,Ivec = rep(40,1),Jvec = c(16000)), 1000)
+  result = nulsim(10, 0.6, 0.1, 16000, generate_spat_abund(theta = 200,Ivec = rep(40,1),Jvec = c(16000)), 500)
   expect_true (is.matrix(result))
   expect_true(max (result[1,]) >= 9 && max (result[1,]) <= 10)
   expect_true(min (result[1,])==0)
@@ -20,8 +20,18 @@ test_that("test nulsim", {
 })
 
 test_that("test CWRsim", {
-  tmax=sample(5:10, 1)
+  tmax=sample(7:10, 1)
   result = CWRsim(tmax,0.6 , 0.1, 0.05, 0.1, 0, 0.0005, 16000, generate_spat_abund(theta = 200,Ivec = rep(40,1),Jvec = c(16000)), 500)
+  expect_true (is.matrix(result))
+  expect_true(max (result[1,]) >= tmax-1 && max (result[1,]) <= tmax)
+  expect_true(min (result[1,])==0)
+  expect_true (nrow (result) > 1)
+})
+
+
+test_that("test CWRsim", {
+  tmax=sample(7:10, 1)
+  result = simbvar( tmax, 0.6, 0.1, 16000, 0.05,0, 5, generate_spat_abund(theta = 200,Ivec = rep(40,1),Jvec = c(16000)), 500)
   expect_true (is.matrix(result))
   expect_true(max (result[1,]) >= tmax-1 && max (result[1,]) <= tmax)
   expect_true(min (result[1,])==0)
